5d3c6c08b8d0aa25ee8361566aa118c45504eb4e
wbdana <william.b.dana@gmail.com>
2017-06-29 22:16:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,18 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :home
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      erb :"pirates/show"
+    end
+
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,28 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height, :ships
+
+  @@all = []
+
+  def initialize(params)
+    # binding.pry
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @ships = []
+    @@all << self
+    self.add_ships(params[:ships])
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_ships(params)
+    params.each do |param|
+      @ships << Ship.new(param)
+    end
+  end
+
+
 end
